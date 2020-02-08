18f799fb6670dfcf9b1c5c0c47cddc1197095854
j-blake <j-blake@users.noreply.github.com>
2020-02-07 18:10:43 -0600

    convert dashboard to functional component

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,58 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import styles from './styles';
 import AppBar from './AppBar';
 import MenuDrawer from './MenuDrawer';
 import Main from './Main';
+import styles from './styles';
 
-class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false,
-    };
-  }
+const useStyles = makeStyles(styles);
 
-  handleDrawerOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleDrawerClose = () => {
-    this.setState({ open: false });
-  };
-
-  render() {
-    const { classes } = this.props;
-    const { open } = this.state;
-
-    return (
-      <BrowserRouter>
-        <div className={classes.root}>
-          <CssBaseline />
-          <AppBar
-            classes={classes}
-            handleDrawerOpen={this.handleDrawerOpen}
-            open={open}
-          />
-          <MenuDrawer
-            classes={classes}
-            handleDrawerClose={this.handleDrawerClose}
-            open={open}
-          />
-          <Main classes={classes} />
-        </div>
-      </BrowserRouter>
-    );
-  }
+export default function Dashboard() {
+  const classes = useStyles();
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  return (
+    <BrowserRouter>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          classes={classes}
+          handleDrawerOpen={() => setIsDrawerOpen(true)}
+          open={isDrawerOpen}
+        />
+        <MenuDrawer
+          classes={classes}
+          handleDrawerClose={() => setIsDrawerOpen(false)}
+          open={isDrawerOpen}
+        />
+        <Main classes={classes} />
+      </div>
+    </BrowserRouter>
+  );
 }
-
-Dashboard.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Dashboard);
