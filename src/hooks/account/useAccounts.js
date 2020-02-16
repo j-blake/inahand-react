df2dc0977081a52ad4a1a15461bc44f1952400f9
j-blake <j-blake@users.noreply.github.com>
2020-02-16 16:25:04 -0600

    Remove useEffect from useAccounts hook - prefetch logic in actions handles when to fetch

--- src/hooks/account/useAccounts.js
+++ src/hooks/account/useAccounts.js
@@ -1,14 +1,8 @@
-import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import * as actions from '../../data/actions/accountActions';
 
 export default function useAccounts() {
   const dispatch = useDispatch();
-  useEffect(() => {
-    async function fetchAccounts() {
-      dispatch(actions.fetchAccounts());
-    }
-    fetchAccounts();
-  });
+  dispatch(actions.fetchAccounts());
   return useSelector((state) => state.account.accounts);
 }
