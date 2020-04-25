bfb2c0d279a9ea000539617a04aa09cea890c30b
j-blake <j-blake@users.noreply.github.com>
2020-04-25 13:01:17 -0500

    Use axios in accounts hooks and services

--- src/hooks/account/useAccountCreate.js
+++ src/hooks/account/useAccountCreate.js
@@ -2,19 +2,13 @@ import { useDispatch } from 'react-redux';
 import { requestAddNewAccount, receiveAddNewAccount, accountsFailed } from '../../data/actions/accountActions';
 import addAccount from '../../data/services/account/addAccount';
 
-const handleFailedResponse = (dispatch) => dispatch(accountsFailed());
-
 export default function useAccountCreate() {
   const dispatch = useDispatch();
   return async (data) => {
     dispatch(requestAddNewAccount(data));
     try {
       const response = await addAccount(data);
-      if (response.ok) {
-        const json = await response.json();
-        return dispatch(receiveAddNewAccount(json));
-      }
-      return (handleFailedResponse(dispatch));
+      return dispatch(receiveAddNewAccount(response.data));
     } catch (error) {
       return dispatch(accountsFailed());
     }
